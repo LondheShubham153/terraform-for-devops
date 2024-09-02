a23e39448fbdccdace6a79b6f903e156a9a62099
Shubham Londhe <shubhamnath5@gmail.com>
2024-09-02 12:12:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region where resources will be provisioned"
+  default     = "us-east-2"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-085f9c64a9b75eed5"
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  default     = "t2.micro"
+}
