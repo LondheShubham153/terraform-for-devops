8f9da01fbf2123314a9b2edc4db3193dc551417d
Shubham Londhe <shubhamnath5@gmail.com>
2024-09-02 12:13:30 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,63 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "terra-automate-key"
+  public_key = file("terra-key.pub")
+}
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "allow_user_to_connect" {
+  name        = "allow TLS"
+  description = "Allow user to connect"
+  vpc_id      = aws_default_vpc.default.id
+  ingress {
+    description = "port 22 allow"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = " allow all outgoing traffic "
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "port 80 allow"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "port 443 allow"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mysecurity"
+  }
+}
+
+resource "aws_instance" "testinstance" {
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.allow_user_to_connect.name]
+  tags = {
+    Name = "Terra-Automate"
+  }
+  root_block_device {
+    volume_size = 10 
+    volume_type = "gp3"
+  }
+}
